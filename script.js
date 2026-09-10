f61753dab3b98a2d3765a77333b0af57369aaa1d
dvnex0 <mattosdavi092@gmail.com>
2026-09-10 20:09:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+let currentChangelog = "android";
+
 function showChange(type) {
+    currentChangelog = type;
+
     const android = document.getElementById("androidChanges");
     const pc = document.getElementById("pcChanges");
 
     if (type === "pc") {
         android.style.display = "none";
         pc.style.display = "block";
-        window.open("https://github.com/dvnex0/DVNexPC/releases/tag/BrowserPC", "_blank");
     } else {
         pc.style.display = "none";
         android.style.display = "block";
-        window.open("https://github.com/DVTech0/DVNex/releases/tag/Browser", "_blank");
+    }
+}
+
+function openOfficialChangelog() {
+    if (currentChangelog === "pc") {
+        window.open(
+            "https://github.com/dvnex0/DVNexPC/releases/tag/BrowserPC",
+            "_blank"
+        );
+    } else {
+        window.open(
+            "https://github.com/DVTech0/DVNex/releases/tag/Browser",
+            "_blank"
+        );
     }
 }
